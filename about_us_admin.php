6038dce17e46b8204dad67115bda537f8a4e95c5
Natalieefd <93095344+Natalieefd@users.noreply.github.com>
2022-11-14 21:00:59 +0800

    done

---
+++ about_us_admin.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style_aboutus.css" />
+    <title>Feel My Bag</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <h1 id="FMBW">Feel My Bag</h1>
+        </div>
+        <div class="menu">
+            <li><a href="halaman_admin.php">home</a></li>
+            <li><a href="product_admin.php">product</a></li>
+        </div>
+    </div>
+    <div class="container">
+        <div class="content">
+            <div class="main-content">
+                <img src="logo.ico" alt="">
+                <p><b><i>Feel My Bag</i></b> merupakan salah satu website tas online<br>yang memiliki kualitas yang baik dengan harga terjangkau.
+                    <br>kami menyediakan berbagai macam jenis tas<br>baik untuk laki-laki maupun perempuan.
+                    <br><br>
+                    Adapun team dari Feel My Bag:<br><br>
+                    1. Agustina Dwi Maharani (2109106037)<br>
+                    2. Natalie Fuad (2109106040)<br>
+                    3. Sherina Laraswati (2109106050)<br><br><br>
+                    <i>please enjoy shopping in our store~~</i>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="outer-footer">
+        Copyright &copy; 2022 Designed by Feel My Bag Team
+    </div>
+</body>
+</html>
