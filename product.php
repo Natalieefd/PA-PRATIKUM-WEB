6885f1719cd17c21457b4433be9264f06e3b0845
Natalieefd <93095344+Natalieefd@users.noreply.github.com>
2022-11-14 20:59:25 +0800

    done

--- product.php
+++ product.php
@@ -1,25 +1,15 @@
-<?php
-   session_start();
 
-   include 'koneksi.php';
-   if(!isset($_SESSION['login'])){
-       echo "<script>
-               alert('Akses ditolak, silahkan login dulu');
-               document.location.href = 'register.php';
-           </script>";
-   }else{
-       $username = $_SESSION['register'];
-   }
+<?php
+session_start();
 
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="logo.ico" type="image/x-icon">
     <link rel="stylesheet" href="style_product.css" />
     <title>Feel My Bag</title>
 </head>
@@ -30,37 +20,28 @@
         </div>
         <div class="menu">
             <li><a href="halaman_user.php">home</a></li>
-            <li><a href="product.php">product</a></li>
             <li><a href="keranjang.php">keranjang</a></li>
             <li><a href="tampil_struk.php">struk</a></li>
-            <li><a href="aboutus.html">about us</a></li>
+            <li><a href="about_us.php">about us</a></li>
         </div>
     </div>
     <div class="search-box">
         <div class="search">
-                <input type="text" name="search" placehoder="search here...." title="search" class="serach-inpt" value="<?php if(isset($_GET['search'])){ echo $_GET['search'];}?>">
+            <input type="text" name="search" placehoder="search here...." title="search" class="serach-inpt" value="<?php if(isset($_GET['search'])){ echo $_GET['search'];}?>">
         </div>
     </div>
-
+ 
     <div class="container">
-        <div class="kategori">
-            <div class="kat-text"><h3>Kategori</h3></div>
-            <div class="kategori-box">
-                <?php
-                    $kategori = mysqli_query($db, "SELECT * FROM kategori ORDER BY id_kategori");
-                    if($kategori->num_rows > 0){
-                        while($l = mysqli_fetch_array($kategori)){
-                ?>
-                <img src="icon_category.png" alt=""  width="70px">
-                <p class="kat"><?php echo $l['id_kategori'] ?></p>
-                <?php }} ?>
-            </div>
-        </div>
-
         <div class="section">
             <div class="content">
                 <div class="box">
                     <?php
+                        include 'koneksi.php';
+
+                        // if (isset($_GET['search'])){
+                        //     $kategori = mysqli_query($db, "SELECT * FROM kategori WHERE nama_kategori LIKE '%".
+                        //         $_GET['search']."%'");
+
                         $product = mysqli_query($db, "SELECT * FROM fmb WHERE jumlah_tas > 0");
                         if(mysqli_num_rows($product) > 0){
                             while($p= mysqli_fetch_array($product)){
@@ -74,13 +55,16 @@
                                     <p class="harga"><?php echo $p['harga'] ?></p>
                                 </div>
                                 <div class="button">
-                                    <div class="add"><a href="#"><i class="fa fa-shopping-cart">keranjang</i></a></div>
-                                    <div class="buy"><a href="#"><i class="fa-solid fa-bag-shopping">beli sekarang</i></a></div>
+                                
+                                    <div class="add"><a href="keranjang.php?cart=<?=$p['id_tas']?>"><i class="fa fa-shopping-cart">keranjang</i></a></div>
+                                    <input type="hidden" name="tanggal_order" value= <?=date("Y-m-d H:i:s a")?> >
+                                    <div class="buy"><a href="tampil_struk.php?buy=<?=$p['id_tas']?>"><i class="fa-solid fa-bag-shopping">beli sekarang</i></a></div>
                                 </div>
                             </div>
                         </a>
-                    <?php }}else{ ?>
-                        <p>Produk tidak ada</p>
+                    <?php 
+                        }}else{ ?>
+                        <br><p>Produk tidak ada</p><br>
                     <?php } ?>
                 </div>
             </div>
@@ -90,5 +74,4 @@
         Copyright &copy; 2022 Designed by Feel My Bag Team
     </div>
 </body>
-
 </html>
