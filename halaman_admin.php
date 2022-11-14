fad2b9e417de3f200aa0996f9354b895cfb8b27a
agustinadwimaharani <93140300+agustinadwimaharani@users.noreply.github.com>
2022-11-14 18:31:04 +0800

    a

--- halaman_admin.php
+++ halaman_admin.php
@@ -17,31 +17,32 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style1.css">
+    <link rel="icon" href="logo.ico" type="image/x-icon">
     <title>Feel My Bag</title>
 </head>
 
 <body>
-    <section>
-        <header class="container">
-            <div class="wrapper">
-                <div class="header">
-                    <h1 id="FMBW">Feel My Bag</h1>
-                </div>
-                <div class="menu">
-                    <li><a href="product.php">Product</a></li>
-                    <li><a href="hasil.php">Data Produk</li>
-                    <li><a href="kategori.php">Data kategori</a></li>
-                    <li><a href="struct.php">Struk</a></li>
-                    <li><a href="logout.php">LOGOUT</a></li>
-                </div>
-            </div>
-                <img src="moon.png" id="icon">
-            </nav>
+    <div class="wrapper">
+        <div class="header">
+            <h1 id="FMBW">Feel My Bag</h1>
+        </div>
+        <div class="menu">
+            <li><a href="product_admin.php">Product</a></li>
+            <li><a href="hasil.php">Data Produk</a></li>
+            <li><a href="kategori.php">Data kategori</a></li>
+            <li><a href="struct.php">Struk</a></li>
+            <li><a href="logout.php">LOGOUT</a></li>
+        </div>
+    </div>
+    <div class="container">
+    <img src="moon.png" id="icon">
+        <div class="main-content">
             <div class="header_main">
                 <div class="header_main_content">
-                    <h1 class="header_main_content">Guess</h1>
-                    <p class="header_main_content">
+                    <h1>Guess</h1>
+                    <p>
                         Malia Convertible Crossbody Flap Bag
                     <ul>
                         <li>-Quilted convertible flap bag with monogram brand print</li>
@@ -52,23 +53,37 @@
                         <li><b>Rp. 1.350.000</b></li>
                     </ul>
                     </p>
-                    <button class="cta">Buy Now</button>
-                    <img src="image4.png" alt="" width="100%" class="header_main_content_img">
+                    <button class="cta" onclick="return tidak_ada()">Buy Now </button>
                 </div>
-                <ul class="thumb">
-                    <li><img src="image4.1.png" onclick="imgSlinder('image4.1.png')"></li>
-                    <li><img src="image4.2.png" onclick="imgSlinder('image4.2.png')"></li>
-                    <li><img src="image4.png" onclick="imgSlinder('image4.png')"></li>
-                </ul>
-                <ul class="sci">
-                    <li><a href="#"><img src="facebook.png" alt=""></a></li>
-                    <li><a href="#"><img src="twitter.png" alt=""></a></li>
-                    <li><a href="#"><img src="instagram.png" alt=""></a></li>
-                </ul>
-    </section>
+            </div>
+            <img class="image" src="image4.png" alt="" width="100%">
+        </div>
+        <ul class="thumb">
+            <li><img src="image4.1.png" onclick="imgSlinder('image4.1.png')"></li>
+            <li><img src="image4.2.png" onclick="imgSlinder('image4.2.png')"></li>
+            <li><img src="image4.png" onclick="imgSlinder('image4.png')"></li>
+        </ul>
+    </div>
+    
+    <div class="footer">
+        <ul class="footer-sosmed">
+            <ul class="footer-brand">
+                <div class="brand">Feel My Bag</div>
+            </ul>
+            <ul class="footer-about1">
+                <li><a href="https://www.facebook.com/"><i class="fa fa-facebook"></i> Facebook</a></li>
+                <li><a href="https://twitter.com/"><i class="fa fa-twitter"></i> Twitter</a></li>
+                <li><a href="#https://www.instagram.com/"><i class="fa fa-instagram"></i> Instagram</a></li>
+            </ul>
+        </ul>
+    <div class="footer-c">
+        Copyright &copy; 2022 Designed by Feel My Bag Team
+    </div>
+    </div>
+
     <script type="text/javascript">
         function imgSlinder(anything){
-            document.querySelector('.header_main_content_img').src =anything;
+            document.querySelector('.image').src =anything;
         }
         var icon = document.getElementById("icon");
         icon.onclick = function () {
@@ -84,23 +99,16 @@
                 icon.src = "moon.png";
                 document.getElementById("FMBW").style.color = "black";
             };
-    
+
         }
 
         function tidak_ada(){ 
-            alert("Maaf belum bisa");
+            alert("Maaf Produk Belum launching");
         }
 
         function warna_nama(){
             document.getElementById("FMBW").style.color = "rgb(162, 127, 127)";
         }
-        </script>
-    <footer>
-        Copyright &copy; 2022
-        Designed by Agustina Dwi Maharani
-    </footer>
-    </div>
-    </header>
+    </script>
 </body>
-
 </html>
