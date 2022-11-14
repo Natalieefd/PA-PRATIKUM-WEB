8478443fec738a0e7ef3e8fbb5cf5c0833b9f774
Natalieefd <93095344+Natalieefd@users.noreply.github.com>
2022-11-14 21:09:58 +0800

    done

--- halaman_user.php
+++ halaman_user.php
@@ -1,3 +1,15 @@
+<?php
+   session_start();
+   if(!isset($_SESSION['registrasi'])){
+       echo "<script>
+               alert('Akses ditolak, silahkan login dulu');
+               document.location.href = 'index.php';
+           </script>";
+   }else{
+       $username = $_SESSION['registrasi'];
+   }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,39 +17,30 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style3.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style3.css">
     <title>Feel My Bag</title>
 </head>
 
 <body>
-    <section>
-    
-    <h1 id  = "nama">Feel My Bag</h1>
-    <script>
-        document.getElementById("nama").addEventListener("mouseenter", mouseEnter);
-        document.getElementById("nama").addEventListener("mouseleave", mouseLeave);
-
-        function mouseEnter(){
-            document.getElementById("nama").style.color="pink";
-        }
-
-        function mouseLeave(){
-            document.getElementById("nama").style.color = "black";
-        }
-    </script>
-        <header class="container">
-            <nav>
-                <ul>
-                    <li><a href="product.php">Product</a></li>
-                    <li><a href="aboutus.html">About us</a></li>
-                    <li><a href="index.php">Logout</a></li>
-                </ul>
-                <img src="moon.png" id="icon">
-            </nav>
+    <div class="wrapper">
+        <div class="header">
+            <h1 id="FMBW">Feel My Bag</h1>
+        </div>
+        <div class="menu">
+            <li><a href="product.php" title="Product">Product</a></li>
+            <li><a href="about_us.php" title="About Us">About us</a></li>
+            <li><a href="index.php" title="Log Out">Logout</a></li>
+        </div>
+    </div>
+    <div class="container">
+    <img src="moon.png" id="icon">
+        <div class="main-content">
             <div class="header_main">
                 <div class="header_main_content">
-                    <h1 class="header_main_content">Guess</h1>
-                    <p class="header_main_content">
+                    <h1>Guess</h1>
+                    <p>
                         Malia Convertible Crossbody Flap Bag
                     <ul>
                         <li>-Quilted convertible flap bag with monogram brand print</li>
@@ -48,31 +51,45 @@
                         <li><b>Rp. 1.350.000</b></li>
                     </ul>
                     </p>
-                    <button class="cta">Buy Now</button>
-                    <img src="image4.png" alt="" width="100%" class="header_main_content_img">
+                    <button class="cta" onclick="return tidak_ada()">Buy Now </button>
                 </div>
-                <ul class="thumb">
-                    <li><img src="image4.1.png" onclick="imgSlinder('image4.1.png')"></li>
-                    <li><img src="image4.2.png" onclick="imgSlinder('image4.2.png')"></li>
-                    <li><img src="image4.png" onclick="imgSlinder('image4.png')"></li>
-                </ul>
-                <div class="sci">
-                <ul>
-                    <li><a href="#"><img src="facebook.png" alt=""></a></li>
-                    <li><a href="#"><img src="twitter.png" alt=""></a></li>
-                    <li><a href="#"><img src="instagram.png" alt=""></a></li>
-                </ul>
             </div>
-    </section>
+            <img class="image" src="image4.png" alt="" width="100%">
+        </div>
+        <ul class="thumb">
+            <li><img src="image4.1.png" onclick="imgSlinder('image4.1.png')"></li>
+            <li><img src="image4.2.png" onclick="imgSlinder('image4.2.png')"></li>
+            <li><img src="image4.png" onclick="imgSlinder('image4.png')"></li>
+        </ul>
+    </div>
+    
+    <div class="footer">
+        <ul class="footer-sosmed">
+            <ul class="footer-brand">
+                <div class="brand">Feel My Bag</div>
+            </ul>
+            <ul class="footer-about1">
+                <li><a href="https://www.facebook.com/"><i class="fa fa-facebook"></i> Facebook</a></li>
+                <li><a href="https://twitter.com/"><i class="fa fa-twitter"></i> Twitter</a></li>
+                <li><a href="#https://www.instagram.com/"><i class="fa fa-instagram"></i> Instagram</a></li>
+            </ul>
+        </ul>
+    <div class="footer-c">
+        Copyright &copy; 2022 Designed by Feel My Bag Team
+    </div>
+    </div>
+
     <script type="text/javascript">
         function imgSlinder(anything){
-            document.querySelector('.header_main_content_img').src =anything;
+            document.querySelector('.image').src =anything;
         }
         var icon = document.getElementById("icon");
         icon.onclick = function () {
             document.body.classList.toggle("dark-theme");
+
             var x = document.getElementById("icon").src;
             var y = x.replace(/^.*[\\\/]/, ''); 
+
             if(y == "moon.png"){
                 icon.src = "sun.png";
                 document.getElementById("FMBW").style.color = "white";
@@ -80,17 +97,16 @@
                 icon.src = "moon.png";
                 document.getElementById("FMBW").style.color = "black";
             };
-    
+
         }
+
+        function tidak_ada(){ 
+            alert("Maaf Produk Belum launching");
+        }
+
         function warna_nama(){
             document.getElementById("FMBW").style.color = "rgb(162, 127, 127)";
         }
-        </script>
-    <footer>
-        Copyright &copy; 2022
-        Designed by Feel My Bag Team
-    </footer>
-    </div>
-    </header>
+    </script>
 </body>
 </html>
