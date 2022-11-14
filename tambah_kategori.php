0fae195ae87170ae2fee3991a7064a19a1a15001
agustinadwimaharani <93140300+agustinadwimaharani@users.noreply.github.com>
2022-11-14 18:26:48 +0800

    a

--- tambah_kategori.php
+++ tambah_kategori.php
@@ -18,6 +18,7 @@
         <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
         <title>Feel My Bag</title>
         <link rel = "stylesheet" href = "style_kategori.css">
+        <link rel="icon" href="logo.ico" type="image/x-icon">
     </head>
 
     <body>
@@ -35,23 +36,29 @@
     <div class="section">
         <div class="container">
         <a href="kategori.php" style="padding:0.4% 0.8%; background-color:#009900;color:#fff;border-radius:2px;text-decoration:none;">Tambah Data Kategori Feel My Bag</a><br>
-            <h2>Tambah Data Kategori</h2>
-            <div class="box">
+            <!-- <h2>Tambah Data Kategori</h2> -->
+            <div class="box2">
                 <form action="" method="POST" enctype = "multipart/form-data">
                     <table>
-                        <tr>   
+                        <center>
+                        <tr >   
                             <td>Nama Kategori</td>
                             <td><input type="text" name="nama_kategori" placeholder="Nama Kategori" class="input_control" required></td>
                         </tr>
                         <tr>   
                         <br>
                             <td><button type="submit" name="submit" value="submit">Submit </button></td>
+                        </center>
+                        <tr >   
                 </table>
             </div>
         </div>
     </div>
-    </form>
-    </body>
+    <div class="footer">
+        Copyright &copy; 2022 Designed by Feel My Bag Team
+    </div>
+</body>
+
 </html>
 
 <?php
